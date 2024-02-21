1e4252073e50af0b5d00350cde0e4ee5faf5690c
Sam Rice <samsrice@gmail.com>
2024-02-20 18:02:35 -0800

    preserve browse route structure

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -5,6 +5,7 @@ import PageContainer from "@/components/PageContainer"
 import { FC, useState } from "react"
 import { DM_Serif_Display } from "next/font/google"
 import { twMerge } from "tailwind-merge"
+import { useRouter } from "next/navigation"
 
 const dmSerifDisplay = DM_Serif_Display({ weight: "400", subsets: ["latin"] })
 
@@ -13,15 +14,16 @@ type SearchCategory = "authors" | "books"
 const BrowsePage: FC = () => {
   const [query, setQuery] = useState("")
   const [category, setCategory] = useState<SearchCategory | "">("")
+  const router = useRouter()
 
-  const submit = () => console.log(`submitting search for "${query}" in ${category}`)
+  const submit = () => router.push(`q=${query}`)
 
   return (
     <PageContainer className=" items-center justify-center">
-      <h1 className={twMerge(dmSerifDisplay.className, "mb-10 text-4xl")} id="search-library">SEARCH LIBRARY</h1>
+      <h1 className={twMerge(dmSerifDisplay.className, "mb-10 text-4xl")} id="search-library">BROWSE LIBRARY</h1>
       <div className="flex h-72 w-full flex-col items-center justify-center space-y-7 bg-theme-beige-400">
         <input
-          className="h-11 w-1/2 min-w-64 pl-3"
+          className="h-11 w-1/2 min-w-64 pl-3 mt-3"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           aria-labelledby="search-library"
